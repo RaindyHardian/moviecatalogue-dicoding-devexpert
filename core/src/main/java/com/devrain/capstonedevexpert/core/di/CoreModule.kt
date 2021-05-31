5fe95c1ae56a911948014fd56f5a5d89bb5a1e29
Raindy Hardiansyah <raindyhardian@gmail.com>
2021-05-31 20:56:30 +0700

    add certificate pinning

--- core/src/main/java/com/devrain/capstonedevexpert/core/di/CoreModule.kt
+++ core/src/main/java/com/devrain/capstonedevexpert/core/di/CoreModule.kt
@@ -6,6 +6,7 @@ import com.devrain.capstonedevexpert.core.domain.repository.IMovieRepository
 import com.devrain.capstonedevexpert.core.utils.AppExecutors
 import net.sqlcipher.database.SQLiteDatabase
 import net.sqlcipher.database.SupportFactory
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -29,10 +30,17 @@ val databaseModule = module {
 
 val networkModule = module {
     single {
+        val hostname = "api.themoviedb.org"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/+vqZVAzTqUP8BGkfl88yU7SQ3C8J2uNEa55B7RZjEg0=")
+            .add(hostname, "sha256/JSMzqOOrtyOT1kmau6zKhgT676hGgczD5VMdRMyJZFA=")
+            .add(hostname, "sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=")
+            .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
